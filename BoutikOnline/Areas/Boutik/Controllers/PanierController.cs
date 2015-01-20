7480214d1aa8f0937a0699341d55ad40efa0fe41
Gracy33 <gracemingiedi@gmail.com>
2015-01-20 16:11:32 +0100

    Modif

---
+++ BoutikOnline/Areas/Boutik/Controllers/PanierController.cs
@@ -0,0 +1,98 @@
+﻿using AdopteUneDev.DAL;
+using BoutikOnline.Areas.Boutik.Models;
+using BoutikOnline.Areas.Membre.Models;
+using BoutikOnline.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BoutikOnline.Areas.Boutik.Controllers
+{
+    public class PanierController : Controller
+    {
+        //
+        // GET: /Boutik/Panier/
+        public ActionResult Index()
+        {
+            return View("AddToBasket", SessionTools.Panier);
+        }
+
+        [HttpPost]
+        public ActionResult AddToBasket(int id, int qte, int choix)
+        {
+            Developer dave = Developer.getInfo(id);
+            if (SessionTools.Panier.Lignes.Where(li => li.ZeDave.IdDev == id).Count() > 0)
+            {
+                SessionTools.Panier.Lignes.Where(li => li.ZeDave.IdDev == id).FirstOrDefault().Qte += qte;
+            }
+            else
+            {
+                Ligne l = new Ligne() { ZeDave = dave, Qte = qte, Choix = choix };
+                SessionTools.Panier.Lignes.Add(l);
+            }
+            return View("AddToBasket", SessionTools.Panier);
+        }
+
+
+        [HttpGet]
+        public ActionResult AddToBasket(int id, int qte, bool op)
+        {
+            if (SessionTools.Panier.Lignes.Where(li => li.ZeDave.IdDev == id).Count() > 0)
+            {
+                if (op)
+                {
+                    SessionTools.Panier.Lignes.Where(li => li.ZeDave.IdDev == id).FirstOrDefault().Qte += qte;
+                }
+                else
+                {
+                    SessionTools.Panier.Lignes.Where(li => li.ZeDave.IdDev == id).FirstOrDefault().Qte -= qte;
+                }
+
+                if (SessionTools.Panier.Lignes.Where(li => li.ZeDave.IdDev == id).FirstOrDefault().Qte < 1) SessionTools.Panier.Lignes.Remove(SessionTools.Panier.Lignes.Where(li => li.ZeDave.IdDev == id).FirstOrDefault());
+            }
+            return View("AddToBasket", SessionTools.Panier);
+        }
+
+        [HttpPost]
+        public ActionResult Supprimer(int id)
+        {
+            //Ligne ligne = null;
+
+            //foreach (Ligne l in SessionTools.Panier.Lignes)
+            //{
+            //    if (l.ZeDave.IdDev == id)
+            //    {
+            //        ligne = l;
+            //    }
+            //}
+            //SessionTools.Panier.Lignes.Remove(ligne);
+
+            /*Version LINQ*/
+            SessionTools.Panier.Lignes.RemoveAll(li => li.ZeDave.IdDev == id);
+
+            return View("AddToBasket", SessionTools.Panier);
+        }
+        [HttpGet]
+        public ActionResult CheckOut()
+        {
+            if (SessionTools.Login == null)
+            {
+                return RedirectToRoute(new { area = "Membre", controller = "Login", action = "LoginForm" }); //Renvoie à la page de login
+            }
+            else
+            {
+                /*Enregistrement du Panier et du Client dans le model PanierUser*/
+                PanierUser checkOut = new PanierUser()
+                {
+                    Panier = SessionTools.Panier,
+                     Client = SessionTools.Client
+                };
+
+                return View("CheckOut", checkOut); //Renvoie à la vue CheckOut
+            }
+
+        }
+    }
+}
---
+++ BoutikOnline/Areas/Membre/Controllers/LoginController.cs
@@ -0,0 +1,69 @@
+﻿using AdopteUneDev.DAL;
+using BoutikOnline.Areas.Boutik.Models;
+using BoutikOnline.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BoutikOnline.Areas.Membre.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Membre/Login/
+        [HttpGet]
+        public ActionResult LoginForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LoginForm(string txtLogin, string txtPassword)
+        {
+            Client cli = Client.AuthentifieMoi(txtLogin, txtPassword);
+            if (cli == null)
+            {
+                ViewBag.ErrorLogin = "Erreur Login/Password";
+                return View();
+            }
+            else
+            {
+                SessionTools.Login = cli.CliUsername;
+                SessionTools.Client = cli;
+                return RedirectToRoute(new { controller = "Home", action = "Index" });
+            }
+        }
+
+        public ActionResult LogOut()
+        {
+            SessionTools.Login = null;
+            Session.Abandon();
+
+            return RedirectToRoute(new { controller = "Home", action = "Index" });
+        }
+
+        [HttpPost]
+        public ActionResult Inscription(string txtName, string txtFirstName, string txtEmail )
+        {
+            Newuser u = new Newuser()
+            {
+                cliName = txtName,
+                cliFirstName = txtFirstName,
+                cliMail = txtEmail
+            };
+                                
+            return View(u);
+        }
+
+        [HttpPost]
+        public ActionResult InscriptionPlus(string txtName, string txtFirstName, string txtEmail, string txtCompany, string txtLogin, string txtPassword)
+        {
+            Client c = new Client();
+            c.saveMe(txtName, txtFirstName, txtEmail, txtCompany, txtLogin, txtPassword);
+            ViewBag.Message = "Vous pouvez vous connecter";
+            return RedirectToRoute(new {area= "Membre", controller = "Login", action = "LoginForm" });
+        }
+    }
+}
